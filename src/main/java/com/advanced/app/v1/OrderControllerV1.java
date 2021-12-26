cf34e2155b3c0a021a4042282a82478693191379
ydh6226 <ydh6226@naver.com>
2021-12-26 21:42:47 +0900

    로그추적기 V1 적용

---
+++ src/main/java/com/advanced/app/v1/OrderControllerV1.java
@@ -0,0 +1,29 @@
+package com.advanced.app.v1;
+
+import com.advanced.app.trace.TraceStatus;
+import com.advanced.app.trace.hellotrace.HelloTraceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV1 {
+
+    private final OrderServiceV1 orderService;
+    private final HelloTraceV1 trace;
+
+    @GetMapping("/v1/request")
+    public String request(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderController.request()");
+            orderService.orderItem(itemId);
+            trace.end(status);
+            return "ok";
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e; // 예외를 꼭 다시 던저줘야한다. 여기서는 로깅만을 위해서 catch함.
+        }
+    }
+}
---
+++ src/main/java/com/advanced/app/v1/OrderRepositoryV1.java
@@ -0,0 +1,37 @@
+package com.advanced.app.v1;
+
+import com.advanced.app.trace.TraceStatus;
+import com.advanced.app.trace.hellotrace.HelloTraceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV1 {
+
+    private final HelloTraceV1 trace;
+
+    public void save(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderRepository.save()");
+            // 저장 로직
+            if (itemId.equals("ex")) {
+                throw new IllegalArgumentException("예외 발생");
+            }
+            sleep(1000);
+
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e; // 예외를 꼭 다시 던저줘야한다. 여기서는 로깅만을 위해서 catch함.
+        }
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException ignored) {
+        }
+    }
+}
