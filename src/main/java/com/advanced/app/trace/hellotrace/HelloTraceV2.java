2275543c5956b11730907f5bebc321923681c865
ydh6226 <ydh6226@naver.com>
2021-12-26 21:49:08 +0900

    로그추적기 V2 - 파라미터 동기화로 개발

---
+++ src/main/java/com/advanced/app/trace/hellotrace/HelloTraceV2.java
@@ -0,0 +1,53 @@
+package com.advanced.app.trace.hellotrace;
+
+import com.advanced.app.trace.TraceId;
+import com.advanced.app.trace.TraceStatus;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class HelloTraceV2 {
+
+    private static final String START_PREFIX = "-->";
+    private static final String COMPLETE_PREFIX = "<--";
+    private static final String EX_PREFIX = "<X-";
+
+    public TraceStatus begin(String message) {
+        TraceId traceId = new TraceId();
+        log.info("[{}] {}{}", traceId.getId(), addSpace(START_PREFIX, traceId.getLevel()), message);
+        return new TraceStatus(traceId, System.currentTimeMillis(), message);
+    }
+
+    public TraceStatus beginSync(TraceId beforeTraceId, String message) {
+        TraceId nextId = beforeTraceId.createNextId();
+        log.info("[{}] {}{}", nextId.getId(), addSpace(START_PREFIX, nextId.getLevel()), message);
+        return new TraceStatus(nextId, System.currentTimeMillis(), message);
+    }
+
+    public void end(TraceStatus status) {
+        complete(status, null);
+    }
+
+    public void exception(TraceStatus status, Exception e) {
+        complete(status, e);
+    }
+
+    private void complete(TraceStatus status, Exception e) {
+        TraceId traceId = status.getTraceId();
+        long resultTimeMs = System.currentTimeMillis() - status.getStartTimeMs();
+        if (e == null) {
+            log.info("[{}] {}{} time={}ms", traceId.getId(), addSpace(COMPLETE_PREFIX, traceId.getLevel()), status.getMessage(), resultTimeMs);
+            return;
+        }
+        log.info("[{}] {}{} time={}ms, ex={}", traceId.getId(), addSpace(EX_PREFIX, traceId.getLevel()), status.getMessage(), resultTimeMs, e.toString());
+    }
+
+    private Object addSpace(String prefix, int level) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < level; i++) {
+            sb.append((i == level - 1) ? "|" + prefix : "|    ");
+        }
+        return sb.toString();
+    }
+}
