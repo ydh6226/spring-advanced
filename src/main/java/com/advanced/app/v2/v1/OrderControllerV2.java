21569d897b075cf8b05c62cb1535e126cda8e7e7
ydh6226 <ydh6226@naver.com>
2021-12-26 21:59:41 +0900

    로그추적기 V2 적용

---
+++ src/main/java/com/advanced/app/v2/v1/OrderControllerV2.java
@@ -0,0 +1,30 @@
+package com.advanced.app.v2.v1;
+
+import com.advanced.app.trace.TraceStatus;
+import com.advanced.app.trace.hellotrace.HelloTraceV1;
+import com.advanced.app.trace.hellotrace.HelloTraceV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV2 {
+
+    private final OrderServiceV2 orderService;
+    private final HelloTraceV2 trace;
+
+    @GetMapping("/v2/request")
+    public String request(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderController.request()");
+            orderService.orderItem(status.getTraceId(), itemId);
+            trace.end(status);
+            return "ok";
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e; // 예외를 꼭 다시 던저줘야한다. 여기서는 로깅만을 위해서 catch함.
+        }
+    }
+}
---
+++ src/main/java/com/advanced/app/v2/v1/OrderServiceV2.java
@@ -0,0 +1,28 @@
+package com.advanced.app.v2.v1;
+
+import com.advanced.app.trace.TraceId;
+import com.advanced.app.trace.TraceStatus;
+import com.advanced.app.trace.hellotrace.HelloTraceV1;
+import com.advanced.app.trace.hellotrace.HelloTraceV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV2 {
+
+    private final OrderRepositoryV2 orderRepository;
+    private final HelloTraceV2 trace;
+
+    public void orderItem(TraceId traceId, String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.beginSync(traceId, "OrderService.orderItem()");
+            orderRepository.save(status.getTraceId(), itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+}
