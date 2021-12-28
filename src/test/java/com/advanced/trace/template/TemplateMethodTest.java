0f7ee0a09ad612d518f6d6cf875ab15400a18501
ydh6226 <ydh6226@naver.com>
2021-12-28 20:37:34 +0900

    템플릿 메소드 패턴 예제

---
+++ src/test/java/com/advanced/trace/template/TemplateMethodTest.java
@@ -0,0 +1,74 @@
+package com.advanced.trace.template;
+
+import com.advanced.trace.template.code.AbstractTemplate;
+import com.advanced.trace.template.code.SubClassLogic1;
+import com.advanced.trace.template.code.SubClassLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class TemplateMethodTest {
+
+    @Test
+    void templateMethod0() {
+        logic1();
+        logic2();
+    }
+
+    private void logic1() {
+        long startTime = System.currentTimeMillis();
+        // 비즈니스 로직 실행
+        log.info("비즈니스 로직1 실행");
+        // 비즈니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+
+    private void logic2() {
+        long startTime = System.currentTimeMillis();
+        // 비즈니스 로직 실행
+        log.info("비즈니스 로직2 실행");
+        // 비즈니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+
+    /**
+     * 템플릿 메서드 패턴 적용
+     */
+    @Test
+    void templateMethod1() {
+        AbstractTemplate template1 = new SubClassLogic1();
+        AbstractTemplate template2 = new SubClassLogic2();
+
+        template1.execute();
+        template2.execute();
+    }
+
+    /**
+     * 템플릿 메서드 패턴 적용2
+     */
+    @Test
+    void templateMethod2() {
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("익명 1");
+            }
+        };
+        log.info("클래스 이름1 = {}", template1.getClass());
+
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("익명 2");
+            }
+        };
+        log.info("클래스 이름1 = {}", template2.getClass());
+
+        template1.execute();
+        template2.execute();
+    }
+}
